e689efe6163135e0f9a5ed6531166e5db44b2d1a
Priya Bhoomireddi <priyagetlucky1011@gmail.com>
2024-07-28 23:53:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-cy.get('.items')
-  .trigger('mousedown', { which: 1, pageX: 493, pageY: 391 })
-  .trigger('mousemove', { pageX: 271, pageY: 391 })
-  .wait(50) // Adding a delay before mouseup
-  .trigger('mouseup', { force: true });
+cy.get('.items').then($items => {
+  const initialScrollLeft = $items[0].scrollLeft;
+  console.log('Initial scrollLeft:', initialScrollLeft);
 
-cy.get('.items').should($items => {
-  expect($items[0].scrollLeft).to.be.greaterThan(0);
+  cy.get('.items')
+    .trigger('mousedown', { which: 1, pageX: 493, pageY: 391 })
+    .trigger('mousemove', { pageX: 271, pageY: 391 })
+    .wait(100) // Increase wait time before mouseup
+    .trigger('mouseup', { force: true });
+
+  cy.get('.items').should($items => {
+    const finalScrollLeft = $items[0].scrollLeft;
+    console.log('Final scrollLeft:', finalScrollLeft);
+    expect(finalScrollLeft).to.be.greaterThan(initialScrollLeft);
+  });
 });
