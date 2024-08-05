5a76945dbbc449395336f8c79a4b7b1f86dd1dc4
Priya Bhoomireddi <priyagetlucky1011@gmail.com>
2024-08-05 23:06:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,11 +2,8 @@ cy.get('.items').then($items => {
   const initialScrollLeft = $items[0].scrollLeft;
   console.log('Initial scrollLeft:', initialScrollLeft);
 
-  
   expect(initialScrollLeft).to.equal(0);
 
-
-  cy.get('.items').then($items => {
   const rect = $items[0].getBoundingClientRect();
   const startX = rect.left + 10; // start 10px from the left edge
   const endX = rect.right - 10; // end 10px from the right edge
@@ -17,10 +14,7 @@ cy.get('.items').then($items => {
     .trigger('mousemove', { pageX: endX, pageY: y })
     .wait(500) 
     .trigger('mouseup', { force: true });
-    .wait(500) 
-    .trigger('mouseup', { force: true });
 
-  
   cy.get('.items').should($items => {
     const finalScrollLeft = $items[0].scrollLeft;
     console.log('Final scrollLeft:', finalScrollLeft);
